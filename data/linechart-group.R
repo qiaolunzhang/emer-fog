87f7bd633bc2d7c76734a97558d30c1d2333c964
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-11-02 23:02:04 +0100

    finish first version of pic

--- data/linechart-group.R
+++ data/linechart-group.R
@@ -8,3 +8,19 @@ head(df2)
 ggplot(data=df2, aes(x=dose, y=len, group=supp)) +
   geom_line(aes(color=supp))+
   geom_point(aes(color=supp))
+
+library(ggplot2)
+
+rain<-read.table("D:\\mycode\\emer-testbed\\data\\graph-size1.csv", header = TRUE,
+                 sep = ",")
+head(rain)
+p<-ggplot(data=rain, aes(x=areaSize, y=longestExecutionTime, group=scheme)) +
+  geom_line(aes(color=scheme))+
+  geom_point(aes(color=scheme))+
+  theme(legend.position = c(.2,.9)) +
+  theme(legend.title = element_blank())
+p<-p+theme(legend.text = element_text(size=22, family = "serif"))
+p<-p+scale_x_continuous("Number of Areas owns per emergency center", breaks = c(50, 100, 150, 200, 250, 300, 350, 400))
+p<-p+theme(axis.text = element_text(size = 22, family = "serif"), axis.title=element_text(size=24, family = "serif"))
+p<-p+scale_y_continuous("Longest Execution Time")
+p
